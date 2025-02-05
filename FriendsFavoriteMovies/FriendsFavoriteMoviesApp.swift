39188fd828053c3aaff91640485fe69d30be60da
Emerson <emerson.day@gmail.com>
2025-02-04 17:22:09 -0800

    Initial Commit

---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Emerson Day on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendsFavoriteMoviesTests/FriendsFavoriteMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesTests.swift
+//  FriendsFavoriteMoviesTests
+//
+//  Created by Emerson Day on 2/4/25.
+//
+
+import Testing
+@testable import FriendsFavoriteMovies
+
+struct FriendsFavoriteMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
