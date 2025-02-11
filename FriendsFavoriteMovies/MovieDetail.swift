493c7830210d301865a11e4927da682fbaed7aa7
Emerson <emerson.day@gmail.com>
2025-02-10 19:49:08 -0800

    context

---
+++ FriendsFavoriteMovies/MovieDetail.swift
@@ -0,0 +1,50 @@
+//
+//  MovieDetail.swift
+//  FriendsFavoriteMovies
+//
+//
+//
+
+
+import SwiftUI
+
+
+struct MovieDetail: View {
+    @Bindable var movie: Movie
+
+
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+
+
+    var body: some View {
+        Form {
+            TextField("Movie title", text: $movie.title)
+
+
+            DatePicker("Release date", selection: $movie.releaseDate, displayedComponents: .date)
+        }
+        .navigationTitle("Movie")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button("Save") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    context.delete(movie)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    NavigationStack {
+        MovieDetail(movie: SampleData.shared.movie)
+    }
+}
--- FriendsFavoriteMovies/MovieList.swift
+++ FriendsFavoriteMovies/MovieList.swift
@@ -1,14 +1,19 @@
 //
 //  MovieList.swift
 //  FriendsFavoriteMovies
+//
+//
+//
 
 
 import SwiftUI
 import SwiftData
 
+
 struct MovieList: View {
     @Query(sort: \Movie.title) private var movies: [Movie]
     @Environment(\.modelContext) private var context
+    @State private var newMovie: Movie?
 
 
     var body: some View {
@@ -16,20 +21,47 @@ struct MovieList: View {
             List {
                 ForEach(movies) { movie in
                     NavigationLink(movie.title) {
-                        Text("Detail view for \(movie.title)")
-                            .navigationTitle("Movie")
-                            .navigationBarTitleDisplayMode(.inline)
+                        MovieDetail(movie: movie)
                     }
                 }
+                .onDelete(perform: deleteMovies(indexes:))
             }
             .navigationTitle("Movies")
+            .toolbar {
+                ToolbarItem {
+                    Button("Add movie", systemImage: "plus", action: addMovie)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    EditButton()
+                }
+            }
+            .sheet(item: $newMovie) { movie in
+                NavigationStack {
+                    MovieDetail(movie: movie)
+                }
+            }
         } detail: {
             Text("Select a movie")
                 .navigationTitle("Movie")
                 .navigationBarTitleDisplayMode(.inline)
         }
     }
+
+
+    private func addMovie() {
+        let newMovie = Movie(title: "", releaseDate: .now)
+        context.insert(newMovie)
+        self.newMovie = newMovie
+    }
+
+
+    private func deleteMovies(indexes: IndexSet) {
+        for index in indexes {
+            context.delete(movies[index])
+        }
+    }
 }
+
 
 #Preview {
     MovieList()
